7c67eb95678aab9bd75ef0092c8c84da26eedcb7
Marmik-001 <marmikrshah@gmail.com>
2024-12-10 14:25:56 +0530

    linked tailwind css
    added static files and added tailwind to the project

--- djbasics/myapp/views.py
+++ djbasics/myapp/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homepage(request):
-
-    return HttpResponse(request.method)
+    return render(request, "index.html")
+def anotherpage(request):
+    return HttpResponse("lmao")
