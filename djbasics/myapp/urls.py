c41e5e52c0893356c51340802c3512f9f1e8bfae
Marmik-001 <marmikrshah@gmail.com>
2024-12-11 20:17:25 +0530

    view for a form
    take user input using post method of forms in html and displaying the data

--- djbasics/myapp/urls.py
+++ djbasics/myapp/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.homepage , name='home'),
-    path('another', views.anotherpage)
+    path('' , views.homepage ),
+    path('home' , views.homepage ),
+    path('about' , views.aboutpage ),
+    path('contact' , views.contactpage ),
+    path('form' , views.formpage),
+    path('formprocess' , views.formprocess),
+    path('marks' , views.marksform),
+    path('resultprocess' , views.resultprocess)
 ]
--- djbasics/myapp/views.py
+++ djbasics/myapp/views.py
@@ -5,5 +5,37 @@ from django.http import HttpResponse
 
 def homepage(request):
     return render(request, "index.html")
-def anotherpage(request):
-    return HttpResponse("lmao")
+
+def aboutpage(request):
+    return render(request, "about.html")
+
+def contactpage(request):
+    return render(request, "contact.html")
+
+def formpage(request):
+    return render(request, "myform.html")
+    
+def formprocess(request):
+    
+    name = request.POST['name']
+    email = request.POST['email']
+    return render(request , "formdata.html"  ,{"name":name , "email":email})
+
+def marksform(request):
+    return render(request , "marksform.html")
+
+def resultprocess(request):
+    print(request.POST)
+    name = request.POST['name']
+    maths = int(request.POST['maths'])
+    sci = int(request.POST['sci'])
+    eng = int(request.POST['eng'])
+    avg = int((maths + sci + eng) / 3)
+    grade = ''
+    if(avg > 90):
+        grade = 'A'
+    elif(avg > 70):
+        grade = 'B'
+    else:
+        grade='FAIL'
+    return render(request, "result.html" , {"name":name , "maths":maths , "sci":sci , "eng":eng , "avg":avg , 'grade':grade})
