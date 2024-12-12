eeda9fed937131cc4408e48acced83d27bfa4628
Marmik-001 <marmikrshah@gmail.com>
2024-12-12 11:26:21 +0530

    added first model

--- djbasics/myapp/models.py
+++ djbasics/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    studentName = models.CharField("Name" , max_length=50 , blank=False)
+    studentMobile = models.CharField('Mobile' , max_length=10  )
+    studentEmail = models.CharField("Email", max_length=30, )
+    studentPassword = models.CharField("Password",  max_length=50  )
+
+
+
+
+
+    def __str__(self):
+        return self.studentName
